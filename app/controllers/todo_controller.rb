bbd21e2e4d179e123e375688d5add4fd5ad113b0
Julien Lery <julienkarllery@gmail.com>
2017-10-01 13:28:20 +0000

    Add conditional logic if statements to the show action of the Todo controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,18 +2,18 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        
+        todo_id = params[:id]
+         # The ... signifies that there is more code in this section, but is not shown in the example for the sake of brevity
+        if todo_id == '1'
         @todo_description = "Make the curriculum"
         @todo_pomodoro_estimate = 4
-        todo_id = params[:id]
 
-        if todo_id == '1' # Remember, the `==` conditional operator is used to check if two things are equal to each other.
-            @todo_description = "Make the curriculum"
-            @todo_pomodoro_estimate = 4
-          
-        elsif todo_id == '2'
-            @todo_description = "Buy workshop supplies"
-            @todo_pomodoro_estimate = 3
+          elsif todo_id == '2'
+          @todo_description = "Buy workshop supplies"
+          @todo_pomodoro_estimate = 3
         end
+       
     end
         
 end
