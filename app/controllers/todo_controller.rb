403ff997f084358e86001125eac56ba3f5665d15
Julien Lery <julienkarllery@gmail.com>
2017-10-01 14:56:12 +0000

    Remove conditional logic and pull data straight from the database

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,17 +2,8 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        
-        todo_id = params[:id]
+         @todo = Todo.find_by_id(params[:id])       
          # The ... signifies that there is more code in this section, but is not shown in the example for the sake of brevity
-        if todo_id == '1'
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 4
-
-          elsif todo_id == '2'
-          @todo_description = "Buy workshop supplies"
-          @todo_pomodoro_estimate = 3
-        end
        
     end
         
@@ -20,3 +11,5 @@ end
 
 
 
+ 
+ 
